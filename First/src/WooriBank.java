1d2ae87f2a5081019056f9a58a791b94668b6613
hayoun <fishingest@kookmin.ac.kr>
2023-04-04 00:37:28 +0900

    [심화] 우리은행 클래스 구현

--- First/src/WooriBank.java
+++ First/src/WooriBank.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class WooriBank {
+public class WooriBank implements Bank{
+
+    int currentMoney;
+    int telephone;
+    int webUrl;
+
+    @Override
+    public void deposit(int money) {
+        this.currentMoney=this.currentMoney+money;
+        System.out.println(money+"만큼 입금되었습니다!");
+    }
+
+    @Override
+    public void withdraw(int money){
+        this.currentMoney=this.currentMoney-money;
+        System.out.println(money+"만큼 출금되었습니다!");
+    };
+    @Override
+    public void checkMoney(){
+        System.out.println("현재 은행에 남은 잔고는 "+this.currentMoney+"입니다!");
+    };
+
 }
