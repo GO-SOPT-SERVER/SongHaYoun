7d1b2b39ffc1af806676ea15cddaddece1cd1389
hayoun <fishingest@kookmin.ac.kr>
2023-04-08 17:49:14 +0900

    [feat] UserService 및 임시 ArrayList 생성

--- SecondSeminar/src/main/java/sopt/org/SecondSeminar/service/UserService.java
+++ SecondSeminar/src/main/java/sopt/org/SecondSeminar/service/UserService.java
@@ -1,2 +1,20 @@
-package sopt.org.SecondSeminar.service;public class UserService {
+package sopt.org.SecondSeminar.service;
+
+import org.springframework.stereotype.Service;
+import sopt.org.SecondSeminar.controller.user.dto.request.RegisterRequestDto;
+import sopt.org.SecondSeminar.domain.User;
+
+import static sopt.org.SecondSeminar.SecondSeminarApplication.userList;
+
+@Service
+public class UserService {
+    public Long register(RegisterRequestDto request){
+
+        User newUser=new User(request.getGender(),request.getName(),request.getContact(),request.getAge());
+
+        userList.add(newUser);
+        newUser.setId((long)userList.size());
+
+        return newUser.getId();
+    }
 }
