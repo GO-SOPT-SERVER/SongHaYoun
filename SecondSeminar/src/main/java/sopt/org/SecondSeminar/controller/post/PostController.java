684a175321b23cb1d65f56cdd40bad0e5fcc7f5e
hayoun <fishingest@kookmin.ac.kr>
2023-04-08 17:50:07 +0900

    [feat] PostController 생성

--- SecondSeminar/src/main/java/sopt/org/SecondSeminar/controller/post/PostController.java
+++ SecondSeminar/src/main/java/sopt/org/SecondSeminar/controller/post/PostController.java
@@ -1,2 +1,31 @@
-package sopt.org.SecondSeminar.controller.post;public class PostController {
+package sopt.org.SecondSeminar.controller.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import sopt.org.SecondSeminar.controller.post.dto.request.PostRequestDto;
+import sopt.org.SecondSeminar.controller.post.dto.response.PostResponseDto;
+import sopt.org.SecondSeminar.domain.Post;
+import sopt.org.SecondSeminar.service.PostService;
+
+import static sopt.org.SecondSeminar.SecondSeminarApplication.postList;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+    @PostMapping("/post")
+    public String register(@RequestBody final PostRequestDto request){
+
+        postService.Post(request);
+
+        return request.getTitle();
+    }
+    @GetMapping("/post/{postId}")
+    public PostResponseDto getPost(@PathVariable final long postId){
+        Post post=postList.get((int) (postId-1));
+        PostResponseDto postResponseDto = PostResponseDto.newInstance(post.getTitle(),post.getCreate_user(),post.getContent());
+        return postResponseDto;
+    }
+
+
 }
