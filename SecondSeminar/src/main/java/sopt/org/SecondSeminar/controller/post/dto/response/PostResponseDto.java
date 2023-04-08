7a8a863fe72216dd9269bbe02ca8d632019254fd
hayoun <fishingest@kookmin.ac.kr>
2023-04-08 17:49:33 +0900

    [feat] Request,Response DTO 생성

--- SecondSeminar/src/main/java/sopt/org/SecondSeminar/controller/post/dto/response/PostResponseDto.java
+++ SecondSeminar/src/main/java/sopt/org/SecondSeminar/controller/post/dto/response/PostResponseDto.java
@@ -1,2 +1,18 @@
-package sopt.org.SecondSeminar.controller.post.dto.response;public class PostResponseDto {
+package sopt.org.SecondSeminar.controller.post.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class PostResponseDto {
+
+    private String title;
+    private String create_user;
+    private String content;
+
+    public static PostResponseDto newInstance(String title,String create_user,String content) {
+        return new PostResponseDto(title,create_user,content);
+    }
 }
