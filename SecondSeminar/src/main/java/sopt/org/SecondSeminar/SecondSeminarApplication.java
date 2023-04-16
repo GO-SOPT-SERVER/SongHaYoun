1d2a6911c71f4a449630a9e43eb76808a77a6be4
hayoun <fishingest@kookmin.ac.kr>
2023-04-17 02:15:19 +0900

    [심화] 임시용 MenuList 생성

--- SecondSeminar/src/main/java/sopt/org/SecondSeminar/SecondSeminarApplication.java
+++ SecondSeminar/src/main/java/sopt/org/SecondSeminar/SecondSeminarApplication.java
@@ -2,6 +2,7 @@ package sopt.org.SecondSeminar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sopt.org.SecondSeminar.domain.Menu;
 import sopt.org.SecondSeminar.domain.Post;
 import sopt.org.SecondSeminar.domain.User;
 
@@ -11,10 +12,12 @@ import java.util.ArrayList;
 public class SecondSeminarApplication {
 	public static ArrayList<User> userList;
 	public static ArrayList<Post> postList;
+	public static ArrayList<Menu> menuList;
 	public static void main(String[] args) {
 		SpringApplication.run(SecondSeminarApplication.class, args);
 		userList=new ArrayList<>();
 		postList=new ArrayList<>();
+		menuList=new ArrayList<>();
 	}
 
 }
