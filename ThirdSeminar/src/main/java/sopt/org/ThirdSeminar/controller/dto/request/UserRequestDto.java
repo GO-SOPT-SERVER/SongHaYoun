91e1612243764a17e4b5e0645f562a379b8433a2
hayoun <fishingest@kookmin.ac.kr>
2023-04-22 17:55:06 +0900

    [feat] 기본 과제

--- ThirdSeminar/src/main/java/sopt/org/ThirdSeminar/controller/dto/request/UserRequestDto.java
+++ ThirdSeminar/src/main/java/sopt/org/ThirdSeminar/controller/dto/request/UserRequestDto.java
@@ -14,17 +14,17 @@ import javax.validation.constraints.Pattern;
 public class UserRequestDto {
     @Email(message = "이메일 형식이 맞지 않습니다.")
     @NotNull
-    private final String email;
+    private String email;
 
     @NotBlank
     @Pattern(regexp = "^[가-힣a-zA-Z]{2,10}$", message = "닉네임 형식에 맞지 않습니다.")
-    private final String nickname;
+    private String nickname;
 
     @NotBlank
     @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
             message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다."
     )
-    private final String password;
+    private String password;
 
 
 }
