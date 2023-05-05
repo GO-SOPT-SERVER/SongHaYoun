b4a44249da35207663ee3b8158579e57d331a97c
hayoun <fishingest@kookmin.ac.kr>
2023-05-05 20:34:34 +0900

    [심화] 등록되지 않은 유저 검색 예외 처리

--- ThirdSeminar/src/main/java/sopt/org/ThirdSeminar/common/advice/ControllerExceptionAdvice.java
+++ ThirdSeminar/src/main/java/sopt/org/ThirdSeminar/common/advice/ControllerExceptionAdvice.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sopt.org.ThirdSeminar.exception.ErrorStatus;
 import sopt.org.thirdSeminar.common.dto.ApiResponseDto;
+
+import javax.persistence.EntityNotFoundException;
+
 @RestControllerAdvice
 public class ControllerExceptionAdvice {
 
@@ -14,6 +17,12 @@ public class ControllerExceptionAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ApiResponseDto handleMethodArgumentNotValidException(final MethodArgumentNotValidException e){
         return ApiResponseDto.error(ErrorStatus.VALIDATION_REQUEST_MISSING_EXCEPTION);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ApiResponseDto handleEntityNotFoundException(final EntityNotFoundException e){
+        return ApiResponseDto.error(ErrorStatus.NOT_FOUND_USER);
     }
 
 }
