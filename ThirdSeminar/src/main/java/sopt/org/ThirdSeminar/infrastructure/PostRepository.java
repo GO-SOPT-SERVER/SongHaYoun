7e0f1a045cee238296c3e8dc33a7a293634b804e
hayoun <fishingest@kookmin.ac.kr>
2023-05-05 22:24:52 +0900

    [심화] PostRepository 정의

--- ThirdSeminar/src/main/java/sopt/org/ThirdSeminar/infrastructure/PostRepository.java
+++ ThirdSeminar/src/main/java/sopt/org/ThirdSeminar/infrastructure/PostRepository.java
@@ -1,2 +1,17 @@
-package sopt.org.ThirdSeminar.infrastructure;public class PostRepository {
+package sopt.org.ThirdSeminar.infrastructure;
+
+
+import org.springframework.data.repository.Repository;
+import sopt.org.ThirdSeminar.domain.Post;
+
+import java.util.List;
+
+public interface PostRepository extends Repository<Post,Long> {
+
+    void save(Post post);
+
+    List<Post> findAllByPostTitle(String title);
+
+    List<Post> findAllById(Long id);
+
 }
