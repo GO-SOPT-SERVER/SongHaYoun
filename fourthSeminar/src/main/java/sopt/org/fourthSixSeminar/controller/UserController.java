eb83b29cbd054272d33513e451b757f63d861059
hayoun <fishingest@kookmin.ac.kr>
2023-05-26 00:32:06 +0900

    [Feat] 로그인 컨트롤러, 서비스 구현

--- fourthSeminar/src/main/java/sopt/org/fourthSixSeminar/controller/UserController.java
+++ fourthSeminar/src/main/java/sopt/org/fourthSixSeminar/controller/UserController.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import sopt.org.fourthSixSeminar.common.dto.ApiResponse;
+import sopt.org.fourthSixSeminar.config.jwt.JwtService;
+import sopt.org.fourthSixSeminar.controller.dto.request.UserLoginRequestDto;
 import sopt.org.fourthSixSeminar.controller.dto.request.UserRequestDto;
+import sopt.org.fourthSixSeminar.controller.dto.response.UserLoginResponseDto;
 import sopt.org.fourthSixSeminar.controller.dto.response.UserResponseDto;
 import sopt.org.fourthSixSeminar.exception.Success;
 import sopt.org.fourthSixSeminar.service.UserService;
@@ -17,10 +20,18 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserService userService;
-
+    private final JwtService jwtService;
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<UserResponseDto> create(@RequestBody @Valid final UserRequestDto request) {
         return ApiResponse.success(Success.SIGNUP_SUCCESS, userService.create(request));
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<UserLoginResponseDto> login(@RequestBody @Valid final UserLoginRequestDto request){
+        final Long userId=userService.login(request);
+        final String token=jwtService.issuedToken(String.valueOf(userId));
+        return ApiResponse.success(Success.LOGIN_SUCCESS,UserLoginResponseDto.of(userId,token));
     }
 }
--- fourthSeminar/src/main/java/sopt/org/fourthSixSeminar/service/UserService.java
+++ fourthSeminar/src/main/java/sopt/org/fourthSixSeminar/service/UserService.java
@@ -3,11 +3,13 @@ package sopt.org.fourthSixSeminar.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sopt.org.fourthSixSeminar.controller.dto.request.UserLoginRequestDto;
 import sopt.org.fourthSixSeminar.controller.dto.request.UserRequestDto;
 import sopt.org.fourthSixSeminar.controller.dto.response.UserResponseDto;
 import sopt.org.fourthSixSeminar.domain.User;
 import sopt.org.fourthSixSeminar.exception.Error;
 import sopt.org.fourthSixSeminar.exception.model.ConflictException;
+import sopt.org.fourthSixSeminar.exception.model.NotFoundException;
 import sopt.org.fourthSixSeminar.infrastructure.UserRepository;
 
 @Service
@@ -31,5 +33,16 @@ public class UserService {
         userRepository.save(newUser);
 
         return UserResponseDto.of(newUser.getId(), newUser.getNickname());
+    }
+    @Transactional
+    public Long login(UserLoginRequestDto request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
+
+        if (!user.getPassword().equals(request.getPassword())) {
+            throw new RuntimeException();
+        }
+
+        return user.getId();
     }
 }
