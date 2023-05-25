911a2321adb20c61d0ea0da496a102adf4ef8139
hayoun <fishingest@kookmin.ac.kr>
2023-05-26 02:04:58 +0900

    [feat] UserId 어노테이션 구현

---
+++ fourthSeminar/src/main/java/sopt/org/fourthSixSeminar/config/resolver/UserIdResolver.java
@@ -0,0 +1,45 @@
+package sopt.org.fourthSixSeminar.config.resolver;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import sopt.org.fourthSixSeminar.config.jwt.JwtService;
+import sopt.org.fourthSixSeminar.domain.User;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotNull;
+
+@RequiredArgsConstructor
+@Component
+public class UserIdResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtService jwtService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter){
+        return parameter.hasParameterAnnotation(UserId.class) && Long.class.equals(parameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(@NotNull MethodParameter parameter, ModelAndViewContainer modelAndViewContainer, @NotNull NativeWebRequest webRequest, WebDataBinderFactory binderFactory){
+        final HttpServletRequest request=(HttpServletRequest) webRequest.getNativeRequest();
+        final String token=request.getHeader("Authorization");
+
+        if(!jwtService.verifyToken(token)){
+            throw new RuntimeException(String.format("UserID를 못가져왔음"));
+        }
+        final String tokenContents=jwtService.getJwtContents(token);
+
+        try{
+            return Long.parseLong(tokenContents);
+        }
+        catch (NumberFormatException e){
+            throw new RuntimeException(String.format("UserId를 가져오지 못했습니다."));
+        }
+    }
+}
--- fourthSeminar/src/main/java/sopt/org/fourthSixSeminar/controller/BoardController.java
+++ fourthSeminar/src/main/java/sopt/org/fourthSixSeminar/controller/BoardController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import sopt.org.fourthSixSeminar.common.dto.ApiResponse;
 import sopt.org.fourthSixSeminar.config.jwt.JwtService;
+import sopt.org.fourthSixSeminar.config.resolver.UserId;
 import sopt.org.fourthSixSeminar.controller.dto.request.BoardRequestDto;
 import sopt.org.fourthSixSeminar.exception.Success;
 import sopt.org.fourthSixSeminar.service.BoardService;
@@ -20,9 +21,9 @@ public class BoardController {
     private final JwtService jwtService;
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public ApiResponse create( @RequestHeader("Authorization") String accessToken,@RequestBody @Valid final BoardRequestDto request) {
+    public ApiResponse create(@UserId Long userId, @RequestBody @Valid final BoardRequestDto request) {
 
-        boardService.create(Long.parseLong(jwtService.getJwtContents(accessToken)),request);
+        boardService.create(userId,request);
         return ApiResponse.success(Success.CREATE_BOARD_SUCCESS);
     }
 }
